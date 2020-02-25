5cbcf95e58f8ced6de29aa1b26dc133716f16f44
Daniel Pereira Castelo Branco <daniel.castelo.branco@ibm.com>
2020-02-25 15:10:42 -0300

    Valida produto antes de inserir na wishlist

---
+++ app/services/helpers/wishlistvalidator.handler.js
@@ -0,0 +1,33 @@
+const mongoose = require('mongoose');
+const wishlistSchema = require('../../models/Wishlist')
+const Wishlist = mongoose.model("WishList", wishlistSchema);
+const productRequest = require('../connections/products.connector').productRequest;
+
+
+const _validateProduct = async (req, res, next) => {
+   
+   const ObjectId = mongoose.Types.ObjectId;
+   const wishlist = await Wishlist.findOne({customer: new ObjectId(req.params.customerId)});
+   
+
+   if (!wishlist){
+      return res.status(404).send({message: "Wishlist not found"});
+   }
+
+   if (wishlist.products.includes(req.body.productId)){
+      return res.status(409).send({message: "Product already added to wishlist"});
+   }
+
+   try{
+      const product = await productRequest(req.body.productId);   
+      if (product) { res.locals.product = product }
+   } catch (err) {
+      return res.status(err.status).send(err);
+   }
+
+   next();
+};
+
+module.exports = {
+   validateProduct: _validateProduct
+}
