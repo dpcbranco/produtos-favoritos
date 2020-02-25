e22b1e19bac1192bcf38c0046c03bbe4ba2f3c62
Daniel Pereira Castelo Branco <daniel.castelo.branco@ibm.com>
2020-02-25 16:18:33 -0300

    Cria API para adição de produto à wishlist

--- app/controllers/wishlist.controller.js
+++ app/controllers/wishlist.controller.js
@@ -13,7 +13,23 @@ const _deleteWishlist = (customerId, callback) => {
    Wishlist.findOneAndRemove({customer: customerId}, (err, wishlistDB) => callback(err, wishlistDB));
 }
 
+const _addProduct = async (req, res) => {
+   const wishlist = res.locals.wishlist;
+   const product = res.locals.product;
+
+   wishlist.products.push(product.id);
+
+   wishlist.save((err, wishlistDB) => {
+      if (err){
+         return res.status(500).send({error: err.message, message: "Error adding favorite product"});
+      }
+
+      return res.status(200).send({message: "Product added successfully"})
+   });
+};
+
 module.exports = {
    newWishlist: _newWishlist,
-   deleteWishlist: _deleteWishlist
+   deleteWishlist: _deleteWishlist,
+   addProduct: _addProduct
 }
