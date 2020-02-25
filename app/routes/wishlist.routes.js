b67d2b07975dde098e06451dd939ba3ebeb6dc63
Daniel Pereira Castelo Branco <daniel.castelo.branco@ibm.com>
2020-02-25 15:22:40 -0300

    Adiciona validador ao adicionar produto

--- app/routes/wishlist.routes.js
+++ app/routes/wishlist.routes.js
@@ -1,6 +1,7 @@
 const router = require('express').Router();
 const wishlistController = require('../controllers/wishlist.controller');
+const wishlistValidator = require('../services/helpers/wishlistvalidator.handler');
 
-router.post('/wishlist/products/add/:customerId', wishlistController.addProduct);
+router.post('/wishlist/products/add/:customerId', wishlistValidator.validateProduct, wishlistController.addProduct);
 
 module.exports = router
