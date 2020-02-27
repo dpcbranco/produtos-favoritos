c618d35a549a96c977faa967c429f0c221c6040d
Daniel Pereira Castelo Branco <daniel.castelo.branco@ibm.com>
2020-02-26 22:21:24 -0300

    Adiciona nova função de validação à API

--- app/routes/wishlist.routes.js
+++ app/routes/wishlist.routes.js
@@ -1,8 +1,8 @@
 const router = require('express').Router();
 const wishlistController = require('../controllers/wishlist.controller');
-const wishlistValidator = require('../services/helpers/wishlistvalidator.handler');
+const productValidator = require('../services/helpers/productvalidator.handler');
 
-router.post('/wishlist/products/add/:productId/customer/:customerId', wishlistValidator.validateProduct, wishlistController.addProduct)
+router.post('/wishlist/products/add/:productId/customer/:customerId', productValidator.validateProduct, productValidator.validateWishListAddition, wishlistController.addProduct)
       .delete('/wishlist/products/remove/:productId/customer/:customerId', wishlistController.removeProduct)
       .get('/wishlist/customer/:customerId', wishlistController.getWishlist);
 
